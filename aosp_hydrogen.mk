362e79deac14ded0f7e24d7e5c1571d4ec6a33df
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-04-01 10:07:43 -0300

    Don't copy apns from here

--- aosp_hydrogen.mk
+++ aosp_hydrogen.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-# Get the long list of APNs
-PRODUCT_COPY_FILES := device/xiaomi/hydrogen/apns-full-conf.xml:system/etc/apns-conf.xml
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
