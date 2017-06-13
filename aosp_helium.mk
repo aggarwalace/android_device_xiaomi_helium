a70263def0654e2b0d4c85348e8542272ce6d030
Sheersh Bansal <aggarwalace@gmail.com>
2017-06-13 14:54:09 +0530

    PN-ify

--- aosp_helium.mk
+++ aosp_helium.mk
@@ -18,18 +18,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from device
-$(call inherit-product, device/xiaomi/hydrogen/device.mk)
+$(call inherit-product, device/xiaomi/helium/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
-PRODUCT_DEVICE := hydrogen
-PRODUCT_NAME := hydrogen
+PRODUCT_DEVICE := helium
+PRODUCT_NAME := helium
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Mi Max
 TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Enable root
+WITH_SU := true
 
 # Use the latest approved GMS identifiers unless running a signed build
 PRODUCT_BUILD_PROP_OVERRIDES += \
